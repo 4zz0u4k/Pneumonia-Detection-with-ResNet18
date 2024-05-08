ec4f4ef193037d5f5b008876819bd2556143b95e
4zz0u4k <la_akeb@esi.dz>
2024-05-08 19:36:23 +0100

    good to go

---
+++ utils.py
@@ -0,0 +1,10 @@
+def save_model(epochs, model, optimizer, criterion, pretrained):
+    """
+    Function to save the trained model to disk.
+    """
+    torch.save({
+                'epoch': epochs,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': criterion,
+                }, f"../outputs/model_pretrained_{pretrained}.pth")
